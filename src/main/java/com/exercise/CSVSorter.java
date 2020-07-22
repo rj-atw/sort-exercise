1205a2b58e3ffe7ee297b84397e7d037d4703cd0
rj <atwal.rj@gmail.com>
2020-07-22 09:36:58 -0700

    Added parallelism to merge step & fix a bad bug in merge

--- src/main/java/com/exercise/CSVSorter.java
+++ src/main/java/com/exercise/CSVSorter.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.Callable;
+import java.util.concurrent.*;
 
 public class CSVSorter implements Callable<Integer> {
 
@@ -21,16 +21,44 @@ public class CSVSorter implements Callable<Integer> {
     @CommandLine.Option(names={"-s"}, description="Number of rows to keep in the buffer")
     private int bufferRows = 2048;
 
-    public File sort() throws IOException, CsvValidationException {
+    @CommandLine.Option(names={"-p"}, description = "Flag to run the algorithm in parallel")
+    private boolean isParallel = false;
+
+    private final ExecutorService executorService = Executors.newWorkStealingPool();
+
+    public File sort() throws IOException, CsvValidationException, ExecutionException, InterruptedException {
         CSVMergeSortHelper mergeSort = new CSVMergeSortHelper(bufferRows, columnIndex1Based-1);
 
         File[] partition = mergeSort.generateSortedFilesWithBufferNumberOfRows(csv);
 
         while ( partition.length != 1) {
-            partition = mergePairsOfFiles(mergeSort, partition);
+            partition = isParallel ? concurrentMergePairsOfFiles(mergeSort, partition) : mergePairsOfFiles(mergeSort, partition);
         }
 
         return partition[0];
+    }
+
+    private File[] concurrentMergePairsOfFiles(final CSVMergeSortHelper mergeSort, File[] partition) throws InterruptedException, ExecutionException {
+        List<Callable<File>> plannedMerges = new LinkedList<>();
+
+        for(int i = 0; i < partition.length; i=i+2) {
+            final int fi = i;
+            if(i+1 < partition.length) {
+                Callable<File> mergedFile = ()-> mergeSort.merge(partition[fi], partition[fi+1]);
+                plannedMerges.add(mergedFile);
+            } else {
+                plannedMerges.add(() -> partition[fi]);
+            }
+        }
+
+        List<Future<File>> outputFutures = executorService.invokeAll(plannedMerges, 10, TimeUnit.MINUTES);
+        List<File> output = new LinkedList<File>();
+
+        for(Future<File> future: outputFutures) {
+            output.add(future.get());
+        }
+
+        return output.toArray(new File[0]);
     }
 
     private File[] mergePairsOfFiles(CSVMergeSortHelper mergeSortHelper, File[] partition) throws IOException, CsvValidationException {
@@ -39,6 +67,8 @@ public class CSVSorter implements Callable<Integer> {
         for(int i =0 ; i < partition.length; i=i+2) {
             if(i+1 < partition.length) {
                 output.add(mergeSortHelper.merge(partition[i], partition[i + 1]));
+            } else {
+                output.add(partition[i]);
             }
         }
         return output.toArray(new File[0]);
@@ -47,6 +77,8 @@ public class CSVSorter implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
+        System.out.println(String.format("Args: %s %d %d " + isParallel, csv.toPath(), columnIndex1Based, bufferRows));
+
         Files.copy(sort().toPath(), System.out);
         return 0;
     }
